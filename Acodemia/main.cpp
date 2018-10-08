2403b90b36780b5f6106f789a08929efca188bee
jackflower <jackflower@poczta.onet.pl>
2018-10-08 12:16:27 +0200

    Update project - Acodemia++

---
+++ Acodemia/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Acodemia++ SCI 2018", sf::Style::Close);
+
+	sf::Texture graphics;
+	graphics.loadFromFile("../data/ship.png");
+
+	sf::Sprite Ship;
+	Ship.setTexture(graphics);
+	Ship.setPosition(300, 300);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+
+		window.clear(sf::Color::White);
+		window.draw(Ship);
+		window.display();
+	}
+
+	return 0;
+}
