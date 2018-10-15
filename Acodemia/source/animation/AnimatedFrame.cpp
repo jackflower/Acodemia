fe7d691b2ea4afe43bafb2e3d162d56082de4793
jackflower <jackflower@poczta.onet.pl>
2018-10-15 16:36:51 +0200

    Update class AnimatedFrame

---
+++ Acodemia/source/animation/AnimatedFrame.cpp
@@ -0,0 +1,83 @@
+//acodemia.pl
+
+#include "AnimatedFrame.h"
+
+namespace acodemia
+{
+	namespace animated
+	{
+		//Konstruktor
+		AnimationFrame::AnimationFrame()
+		:
+			p_texture(0),
+			m_rectangle()
+		{
+		}
+
+		//Konstruktor kopiuj¹cy
+		AnimationFrame::AnimationFrame(const AnimationFrame & copy)
+		:
+			p_texture(copy.p_texture),
+			m_rectangle(copy.m_rectangle)
+		{
+		}
+
+		//Konstruktor przenosz¹cy
+		AnimationFrame::AnimationFrame(AnimationFrame && other)
+		{
+			//to do...
+			//look at class Displayable...
+		}
+
+		//Konstruktor parametryczny
+		AnimationFrame::AnimationFrame(const sf::Texture & texture, const sf::IntRect & rectangle)
+		:
+			p_texture(&texture),
+			m_rectangle(rectangle)
+		{
+		}
+
+		//Konstruktor parametryczny
+		AnimationFrame::AnimationFrame(const sf::Texture & texture)
+		:
+			p_texture(&texture)
+		{
+			if (p_texture != 0)
+				m_rectangle = sf::IntRect(0, 0, p_texture->getSize().x, p_texture->getSize().y);
+		}
+
+		//Destruktor
+		AnimationFrame::~AnimationFrame()
+		{
+			p_texture = nullptr;
+		}
+
+		//Przeci¹¿ony operator przypisania kopiowania
+		AnimationFrame & AnimationFrame::operator=(const AnimationFrame & copy)
+		{
+			// to do...
+			//look at class Displayable...
+			return *this;
+		}
+
+		//Przeci¹¿ony operator przypisania przenoszenia
+		AnimationFrame & AnimationFrame::operator=(AnimationFrame && other)
+		{
+			//to do...
+			//look at class Displayable...
+			return *this;
+		}
+
+		//Metoda zwraca sta³y wskaŸnik na teksturê klatki animacji z wektora
+		const sf::Texture *AnimationFrame::GetFrameImage() const
+		{
+			return p_texture;
+		}
+
+		//Metoda zwraca koordynaty klatki animacji z wektora
+		const sf::IntRect AnimationFrame::GetFrameRectangle() const
+		{
+			return m_rectangle;
+		}
+	}//namespace animated
+}//namespace acodemia
---
+++ Acodemia/source/animation/AnimatedFrame.h
@@ -0,0 +1,94 @@
+//
+//acodemia.pl
+//
+
+#ifndef H_ANIMATION_FRAME_JACK
+#define H_ANIMATION_FRAME_JACK
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+
+namespace acodemia
+{
+	namespace animated
+	{
+		///
+		///Klasa reprezentuje klatkê animacji
+		//
+		class AnimationFrame
+		{
+		public:
+
+			///
+			//Konstruktor domyœlny
+			///
+			AnimationFrame();
+
+			///
+			///Konstruktor kopiuj¹cy
+			///
+			///@param & copy - sta³a referencja na obiekt klasy AnimationFrame
+			///
+			AnimationFrame(const AnimationFrame & copy);
+
+			///
+			///Konstruktor przenosz¹cy
+			///
+			///@param && other = referencja do r-wartoœci
+			///
+			AnimationFrame(AnimationFrame && other);
+
+			///
+			///Konstruktor parametryczny
+			///
+			///@param & texture - tekstura
+			///
+			///@param & rectangle - obszar prostok¹tny tekstury
+			///
+			AnimationFrame(const sf::Texture & texture, const sf::IntRect & rectangle);
+
+			///
+			///Konstruktor parametryczny
+			///
+			///@param & texture - tekstura
+			///
+			AnimationFrame(const sf::Texture & texture);
+			
+			///
+			///Destruktor
+			///
+			~AnimationFrame();
+
+			///
+			///Przeci¹¿ony operator przypisania kopiowania
+			///
+			///@param & copy - sta³a referencja na obiekt klasy AnimationFrame
+			///
+			AnimationFrame & operator=(const AnimationFrame & copy);
+
+			///
+			///Przeci¹¿ony operator przypisania przenoszenia
+			///
+			///@param && other -  referencja do r-wartoœci
+			///
+			AnimationFrame & operator=(AnimationFrame && other);
+			
+
+			///
+			///Metoda zwraca sta³y wskaŸnik na teksturê klatki animacji z wektora
+			///
+			const sf::Texture *GetFrameImage() const;
+
+			///
+			///Metoda zwraca koordynaty klatki animacji z wektora
+			///
+			const sf::IntRect GetFrameRectangle() const;
+
+		private:
+
+			const sf::Texture*	p_texture;		//wskaŸnik na teksturê
+			sf::IntRect			m_rectangle;	//obszar prostok¹tny klatki animacji
+		};
+	}//namespace animated
+}//namespace acodemia
+#endif // H_ANIMATION_FRAME_JACK
