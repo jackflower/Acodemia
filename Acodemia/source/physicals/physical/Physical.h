4b085268f7d06739ec983fb5f6715932de86acba
jackflower <jackflower@poczta.onet.pl>
2018-10-30 10:30:35 +0100

    Update

--- Acodemia/source/physicals/physical/Physical.h
+++ Acodemia/source/physicals/physical/Physical.h
@@ -24,22 +24,18 @@ namespace acodemia
 
 		protected:
 
-			// P h y s i c a l
-			//float					m_circle_radius;			//promieñ detekcji kolizji
-			//ECollisionShape		m_collision_shape;			//wyliczenie - typ detekcji kolizji
-			//sf::Vector2f			m_rect_size;				//wektor przechowuj¹cy rozmiar obszaru prostok¹tnego
 
 		private:
 
-			Displayable		m_displayable;			//reprezentacja graficzna
-			bool			m_ready_for_destruction;//flaga ustawia obiekty gotowe do destrukcji
-			bool			m_use_displayable;		//flaga okreœla, czy obiekt posiada reprezentacjê graficzn¹
-			sf::Vector2f	m_speed;				//wektor prêdkoœci obiektu
-			sf::Vector2f	m_position;				//wektor pozycji
-			float			m_rotation;				//wartoœæ obrotu
-			sf::Vector2f	m_origin;				//wektor uchwytu obiektu
-			sf::Vector2f	m_scale;				//skala
-			sf::Color		m_color;				//kolor obiektu
+			Displayable		m_displayable;		//reprezentacja graficzna
+			bool			m_destruction;		//flaga destrukcji
+			bool			m_use_displayable;	//flaga okreœla, czy obiekt posiada reprezentacjê graficzn¹
+			sf::Vector2f	m_speed;			//wektor prêdkoœci obiektu
+			sf::Vector2f	m_position;			//wektor pozycji
+			float			m_rotation;			//wartoœæ obrotu
+			sf::Vector2f	m_origin;			//wektor uchwytu obiektu
+			sf::Vector2f	m_scale;			//skala
+			sf::Color		m_color;			//kolor obiektu
 											
 			//sf::Transformable m_transformable; // czy to bêdzie potrzebne?
 			//	position
@@ -55,24 +51,28 @@ namespace acodemia
 
 
 /*
+// P h y s i c a l
+//float					m_circle_radius;			//promieñ detekcji kolizji
+//ECollisionShape		m_collision_shape;			//wyliczenie - typ detekcji kolizji
+//sf::Vector2f			m_rect_size;				//wektor przechowuj¹cy rozmiar obszaru prostok¹tnego
 
 //Wirtualna metoda ustawiaj¹ca wartoœæ promienia detekcji wykorzystywan¹ przy liczeniu kolizji
 void CPhysical::SetBoundingCircle(float radius)
 {
-m_collision_shape = SHAPE_CIRCLE;
-m_circle_radius = radius;
-//obliczamy boki prostok¹ta (uproszczenie do kwadratu) wpisanego w okrag
-m_rect_size.x = m_rect_size.y = radius * 2.0f;
+	m_collision_shape = SHAPE_CIRCLE;
+	m_circle_radius = radius;
+	//obliczamy boki prostok¹ta (uproszczenie do kwadratu) wpisanego w okrag
+	m_rect_size.x = m_rect_size.y = radius * 2.0f;
 }
 
 
 //Metoda ustawia wartoœæ obszaru prostok¹tnego detekcji wykorzystywany przy liczeniu kolizji
 void CPhysical::SetBoundingRect(const sf::Vector2f & rect_size)
 {
-m_collision_shape = SHAPE_RECT;
-m_rect_size = rect_size;
-//obliczamy przeciwprostok¹tn¹, czyli œrednicê okrêgu opisanego na prostok¹cie
-m_circle_radius =  sqrtf(rect_size.x * rect_size.x + rect_size.y * rect_size.y);
+	m_collision_shape = SHAPE_RECT;
+	m_rect_size = rect_size;
+	//obliczamy przeciwprostok¹tn¹, czyli œrednicê okrêgu opisanego na prostok¹cie
+	m_circle_radius =  sqrtf(rect_size.x * rect_size.x + rect_size.y * rect_size.y);
 }
 
 
