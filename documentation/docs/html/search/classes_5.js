c876c2758fb10272ae27359bcca19dfcf4720579
jackflower <jackflower@poczta.onet.pl>
2018-11-14 12:29:46 +0100

    Update

--- documentation/docs/html/search/classes_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['texture',['Texture',['../classacodemia_1_1rendering_1_1_texture.html',1,'acodemia::rendering']]]
-];
--- documentation/docs/html/search/defines_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['gphysicalmanager',['gPhysicalManager',['../_physical_manager_8h.html#afdfc70fefff93cb1d5672a4ef559484d',1,'PhysicalManager.h']]]
-];
