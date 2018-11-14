43825c1cd5e1b0312a1eae95e1b3835ba7fde0ec
jackflower <jackflower@poczta.onet.pl>
2018-11-14 10:48:31 +0100

    Update

--- Acodemia/docs/search/classes_4.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['singleton',['Singleton',['../class_singleton.html',1,'']]],
-  ['singleton_3c_20physicalmanager_20_3e',['Singleton&lt; PhysicalManager &gt;',['../class_singleton.html',1,'']]],
-  ['sprite',['Sprite',['../classacodemia_1_1rendering_1_1_sprite.html',1,'acodemia::rendering']]]
-];
--- Acodemia/docs/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['acodemia',['Acodemia',['../md__c_1__users__jacek__source__repos__acodemia__r_e_a_d_m_e.html',1,'']]]
-];
