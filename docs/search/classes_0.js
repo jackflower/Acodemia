f3314a7441af42dc7ad49ab2e9a18a9f78d8677c
jackflower <jackflower@poczta.onet.pl>
2018-11-16 12:54:56 +0100

    Update

--- docs/search/classes_0.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['actor',['Actor',['../classacodemia_1_1physical_1_1_actor.html',1,'acodemia::physical']]],
-  ['animated',['Animated',['../classacodemia_1_1animation_1_1_animated.html',1,'acodemia::animation']]],
-  ['animation',['Animation',['../classacodemia_1_1animation_1_1_animation.html',1,'acodemia::animation']]],
-  ['animationframe',['AnimationFrame',['../classacodemia_1_1animation_1_1_animation_frame.html',1,'acodemia::animation']]]
-];
--- docs/search/files_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['displayable_2ecpp',['Displayable.cpp',['../_displayable_8cpp.html',1,'']]],
-  ['displayable_2eh',['Displayable.h',['../_displayable_8h.html',1,'']]]
-];
